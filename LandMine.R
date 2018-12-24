baa5b8dfcae47a0da49c6ca51270c19d29f9772f
Eliot McIntire <eliotmcintire@gmail.com>
2018-12-24 12:05:02 -0800

    SpaDES.core version minimum

--- LandMine.R
+++ LandMine.R
@@ -7,7 +7,7 @@ defineModule(sim, list(
     person(c("Alex", "M."), "Chubaty", email = "achubaty@friresearch.ca", role = c("ctb"))
   ),
   childModules = character(0),
-  version = numeric_version("0.0.1"),
+  version = list(SpaDES.core = "0.2.3.9009", LandMine = numeric_version("0.0.1")),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
